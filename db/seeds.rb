e7210cf3cfe906dfea8acc698aeb0eda6ccdcbe1
Pedro Ivo Rocha <pedro.rocha@orulo.com.br>
2025-01-28 11:38:13 +0000

    restyle and create seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,37 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+require 'faker'
+
+10.times do
+  Company.find_or_create_by!(
+    name: Faker::Company.name
+  )
+end
+
+Admin.create!(
+    email: 'admin@admin.com',
+    password: 'admin123',
+    password_confirmation: 'admin123'
+  )
+
+10.times do
+  Admin.create!(
+    email: Faker::Internet.email,
+    password: 'password',
+    password_confirmation: 'password'
+  )
+end
+
+Admin.all.each do |admin|
+  Company.all.each do |company|
+    Invite.find_or_create_by!(
+      admin: admin,
+      company: company
+    )
+  end
+end
+
+Invite.last(2).first.update(deactivated_at: 1.month.from_now)
+Invite.last.update(deactivated_at: 2.month.from_now)
